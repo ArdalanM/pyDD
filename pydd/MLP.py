caca5600cb6002ba574998503165dcaf6a54d833
Ardalan <ardalan.mehrani@iosquare.com>
2016-11-23 18:10:25 +0100

    add predict from svm string directly factor fit in parent class

--- pydd/MLP.py
+++ pydd/MLP.py
@@ -112,10 +112,37 @@ class genericMLP(AbstractDDCalls, BaseEstimator):
         with open("{}/model.json".format(self.model['repository'])) as f:
             self.calls = [json.loads(line, encoding='utf-8') for line in f]
 
-    def fit(self, filepaths, iterations=100, test_interval=None, base_lr=0.1,
-            solver_type='SGD', batch_size=128, metrics=['mcll', 'accp'], class_weights=None):
+    def fit(self, X, Y=None, validation_data=[], iterations=100, test_interval=None,
+            solver_type='SGD',
+            base_lr=0.1,
+            lr_policy=None,
+            stepsize=None,
+            momentum=None,
+            weight_decay=None,
+            power=None,
+            iter_size=1,
+            batch_size=128,
+            metrics=['mcll', 'accp'],
+            class_weights=None):
 
-        self.filepaths = [filepaths] if type(filepaths) is not list else filepaths
+        self.filepaths = []
+        if type(X) == np.ndarray:
+            train_f = os.path.join(self.data_folder, "x_train_{}.svm".format(time_utils.fulltimestamp()))
+            dump_svmlight_file(X, Y, train_f)
+            self.filepaths.append(train_f)
+
+            if len(validation_data) > 0:
+                for i, (x_val, y_val) in enumerate(validation_data):
+                    valid_f = os.path.join(self.data_folder, "x_val{}_{}.svm".format(i, time_utils.fulltimestamp()))
+                    dump_svmlight_file(x_val, y_val, valid_f)
+                    self.filepaths.append(valid_f)
+
+        elif type(X) == list:
+            self.filepaths = X
+        elif type(X) == str:
+            self.filepaths = [X]
+        else:
+            raise
 
         # df: True otherwise core dump when training on svm data
         self.train_parameters_input = {'db': True},
@@ -125,7 +152,13 @@ class genericMLP(AbstractDDCalls, BaseEstimator):
             'solver': {'iterations': iterations,
                        'test_interval': test_interval,
                        'base_lr': base_lr,
-                       'solver_type': solver_type},
+                       'solver_type': solver_type,
+                       'lr_policy': lr_policy,
+                       'stepsize': stepsize,
+                       'momentum': momentum,
+                       'weight_decay': weight_decay,
+                       'power': power,
+                       'iter_size': iter_size},
             'net': {'batch_size': batch_size},
             'class_weights': class_weights if class_weights else [1.] * self.service_parameters_mllib['nclasses']
         }
@@ -161,7 +194,29 @@ class genericMLP(AbstractDDCalls, BaseEstimator):
                 print(train_status)
                 break
 
-    def predict_proba(self, svm_filepath):
+    def _to_list_of_svm_strings(self, Xndarray):
+        list_svm_string = []
+        for i in range(Xndarray.shape[0]):
+
+            x = Xndarray[i, :]
+            indexes = x.nonzero()[0]
+            values = x[indexes]
+
+            # where the magic happen :)
+            svm_string = list(map(lambda idx_val: '{}:{}'.format(idx_val[0], idx_val[1]), zip(indexes, values)))
+            svm_string = ' '.join(svm_string)
+            list_svm_string.append(svm_string)
+
+        return list_svm_string
+
+    def predict_proba(self, X):
+
+        data = [X]
+        if type(X) == np.ndarray:
+            # Prediction from numpy array
+            # pred_f = os.path.join(self.data_folder, "x_pred{}_{}.svm".format(self.n_pred, time_utils.fulltimestamp()))
+            # dump_svmlight_file(X, [-1] * X.shape[0], pred_f)
+            data = self._to_list_of_svm_strings(X)
 
         nclasses = self.service_parameters_mllib['nclasses']
         self.predict_parameters_input = {}
@@ -169,7 +224,8 @@ class genericMLP(AbstractDDCalls, BaseEstimator):
                                          "gpuid ": self.service_parameters_mllib['gpuid']}
         self.predict_parameters_output = {'best': nclasses}
 
-        json_dump = self.post_predict(self.sname, [svm_filepath], self.predict_parameters_input,
+
+        json_dump = self.post_predict(self.sname, data, self.predict_parameters_input,
                                       self.predict_parameters_mllib, self.predict_parameters_output)
 
         self.answers.append(json_dump)
@@ -177,6 +233,7 @@ class genericMLP(AbstractDDCalls, BaseEstimator):
             self.calls = [json.loads(line, encoding='utf-8') for line in f]
 
         y_score = to_array(json_dump, nclasses)
+
         return y_score
 
     def predict(self, X):
@@ -275,66 +332,3 @@ class MLPfromArray(genericMLP):
                                            regression=regression,
                                            finetuning=finetuning,
                                            db=db)
-
-    def fit(self, X, Y, validation_data=None, iterations=100, test_interval=None, base_lr=0.1,
-            solver_type='SGD', batch_size=128, metrics=['mcll', 'accp'], class_weights=None):
-
-        self.filepaths = []
-        train_f = os.path.join(self.data_folder, "x_train_{}.svm".format(time_utils.fulltimestamp()))
-        dump_svmlight_file(X, Y, train_f)
-        self.filepaths.append(train_f)
-
-        if validation_data:
-            for i, (x_val, y_val) in enumerate(validation_data):
-                valid_f = os.path.join(self.data_folder, "x_val{}_{}.svm".format(i, time_utils.fulltimestamp()))
-                dump_svmlight_file(x_val, y_val, valid_f)
-                self.filepaths.append(valid_f)
-
-        # df: True otherwise core dump when training on svm data
-        self.train_parameters_input = {'db': True},
-        self.train_parameters_output = {"measure": metrics},
-        self.train_parameters_mllib = {
-            'gpu': self.service_parameters_mllib['gpu'],
-            'solver': {'iterations': iterations,
-                       'test_interval': test_interval,
-                       'base_lr': base_lr,
-                       'solver_type': solver_type},
-            'net': {'batch_size': batch_size},
-            'class_weights': class_weights if class_weights else [1.] * self.service_parameters_mllib['nclasses']
-        }
-
-        self.post_train(self.sname,
-                        self.filepaths,
-                        self.train_parameters_input,
-                        self.train_parameters_mllib,
-                        self.train_parameters_output, async=True)
-        time.sleep(1)
-
-        train_status = ''
-        while True:
-            train_status = self.get_train(self.sname, job=1, timeout=2)
-            if train_status['head']['status'] == 'running':
-                print(train_status['body']['measure'])
-            else:
-                print(train_status)
-                break
-
-    def predict_proba(self, X):
-
-        pred_f = os.path.join(self.data_folder, "x_pred{}_{}.svm".format(self.n_pred, time_utils.fulltimestamp()))
-        dump_svmlight_file(X, [-1] * X.shape[0], pred_f)
-        nclasses = self.service_parameters_mllib['nclasses']
-
-        self.predict_parameters_input = {}
-        self.predict_parameters_mllib = {"gpu": self.service_parameters_mllib['gpu'],
-                                         "gpuid ": self.service_parameters_mllib['gpuid']}
-        self.predict_parameters_output = {'best': nclasses}
-
-        json_dump = self.post_predict(self.sname, [pred_f], self.predict_parameters_input,
-                                      self.predict_parameters_mllib, self.predict_parameters_output)
-        y_score = to_array(json_dump, nclasses)
-        return y_score
-
-    def predict(self, X):
-        y_score = self.predict_proba(X)
-        return (np.argmax(y_score, 1)).reshape(len(y_score), 1)
