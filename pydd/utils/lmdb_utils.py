10522a754a43c0b09987d82c9bb24b869d2b1fcc
Ardalan <ardalan.mehrani@iosquare.com>
2017-03-12 23:29:08 +0100

    new archi, new unit test

--- pydd/utils/lmdb_utils.py
+++ pydd/utils/lmdb_utils.py
@@ -6,17 +6,25 @@
 import os
 import shutil
 import tempfile
-from pydd.LR import LRfromSVM
+from pydd.models import LR
+from pydd.solver import GenericSolver
+from pydd.connectors import SVMConnector
 
 
-def create_lmdb_from_svm(svm_path, lmdb_path, vocab_path=None,
-                         host='localhost',
+def create_lmdb_from_svm(svm_path, lmdb_path, vocab_path=None, host='localhost',
                          port=8085, nclasses=2, gpu=True, tmp_folder=None):
+
+    assert not os.path.exists(lmdb_path)
+    assert not os.path.exists(vocab_path)
 
     tmp_folder = tempfile.mkdtemp(prefix="pydd_", dir=tmp_folder) if tmp_folder else tempfile.mkdtemp(prefix="pydd_")
 
-    clf = LRfromSVM(host=host, port=port, nclasses=nclasses, gpu=gpu, repository=tmp_folder)
-    clf.fit([svm_path], iterations=1)
+    train_data = SVMConnector(path=svm_path)
+    optimizer = GenericSolver(solver_type='SGD', base_lr=0.01, iterations=1)
+
+    clf = LR(host=host, port=port, nclasses=nclasses, gpu=gpu, repository=tmp_folder)
+    clf.fit(train_data, solver=optimizer)
+
     shutil.move(os.path.join(tmp_folder, "train.lmdb"), lmdb_path)
     if vocab_path:
         shutil.move(os.path.join(tmp_folder, "vocab.dat"), vocab_path)
@@ -32,27 +40,41 @@ def create_lmdb_from_svm(svm_path, lmdb_path, vocab_path=None,
 
 if __name__ == "__main__":
 
-    import os
     from sklearn import datasets, preprocessing, model_selection
-    from pydd.MLP import MLPfromSVM
+
+    # parameters
+    port = 8085
+    gpu = True
+    nclasses = 10
+    seed = 1337
+    test_size = 0.2
+    params = {'port': 8085, 'nclasses': nclasses, 'gpu': True}
 
     # Create dataset
-    X, Y = datasets.load_digits(return_X_y=True)
+    X, Y = datasets.load_digits(n_class=params['nclasses'], return_X_y=True)
     X = preprocessing.StandardScaler().fit_transform(X)
-    x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y, test_size=0.2, random_state=1337)
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
 
     # Save data in .svm format
-    train_path = os.path.abspath('x_train.svm')
-    test_path = os.path.abspath('x_test.svm')
-    datasets.dump_svmlight_file(x_train, y_train, train_path)
-    datasets.dump_svmlight_file(x_test, y_test, test_path)
+    tr_svm_f, tr_lmdb_f = os.path.abspath('x_train.svm'), os.path.abspath('x_train.lmdb')
+    te_svm_f, te_lmdb_f = os.path.abspath('x_test.svm'), os.path.abspath('x_test.lmdb')
+    vocab_path = os.path.abspath('vocab.dat')
 
-    lmdb_path, vocab = create_lmdb_from_svm(train_path, os.path.abspath('train.lmdb'), os.path.abspath('vocab.dat'), port=8085, gpu=True)
-    lmdb_path1, _ = create_lmdb_from_svm(test_path, os.path.abspath('test.lmdb'), port=8085, gpu=True)
+    datasets.dump_svmlight_file(x_train, y_train, tr_svm_f)
+    datasets.dump_svmlight_file(x_test, y_test, te_svm_f)
 
-    clf_lmdb = MLPfromSVM(port=8085, nclasses=10, gpu=True)
-    clf_lmdb.fit([train_path, test_path], lmdb_paths=[lmdb_path, lmdb_path1], vocab_path=vocab)
+    # create lmdb and vocab file
+    create_lmdb_from_svm(svm_path=tr_svm_f, lmdb_path=tr_lmdb_f, vocab_path=vocab_path, **params)
+    # create_lmdb_from_svm(svm_path=tr_svm_f, lmdb_path=tr_lmdb_f, **params)
 
-    y_pred_lmdb = clf_lmdb.predict_proba(train_path)
+    tr_data = SVMConnector(path=tr_svm_f, lmdb_path=tr_lmdb_f, vocab_path=vocab_path)
+    te_data = SVMConnector(path=tr_svm_f, lmdb_path=tr_lmdb_f)
+
+    optimizer = GenericSolver(solver_type='SGD', base_lr=0.01, iterations=100)
+
+    clf = LR(**params)
+    clf.fit(tr_data, validation_data=[te_data], solver=optimizer)
+
+    y_pred_lmdb = clf.predict_proba(te_data)
 
 
