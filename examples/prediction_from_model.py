4e17ced0c0ac85caefd2858f78ecd0baa38347f8
Ardalan <ardalan.mehrani@iosquare.com>
2017-03-13 14:06:20 +0100

    update example to match new architecture

---
+++ examples/prediction_from_model.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Ardalan MEHRANI <ardalan77400@gmail.com>
+@brief:
+"""
+import os
+from pydd.utils import os_utils
+from pydd.models import MLP
+from pydd.connectors import SVMConnector, ArrayConnector
+from pydd.solver import GenericSolver
+from sklearn import datasets, preprocessing, model_selection, metrics
+
+
+# Parameters
+test_size = 0.2
+seed = 1337
+snames = 'predict_from_model'
+model_repo = os.path.abspath('trained_model')
+params = {'host': 'localhost', 'port': 8085, 'nclasses': 10, 'layers': [100, 100]}
+
+# We make sure model repo does not exist
+if os.path.exists(model_repo):
+    os_utils._remove_dirs([model_repo])
+os_utils._create_dirs([model_repo])
+
+
+# create dataset
+X, Y = datasets.load_digits(return_X_y=True)
+X = preprocessing.StandardScaler().fit_transform(X)
+xtr, xte, ytr, yte = model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
+
+# create and save train.svm and test.svm
+tr_f = os.path.abspath('x_train.svm')
+te_f = os.path.abspath('x_test.svm')
+datasets.dump_svmlight_file(xtr, ytr, tr_f)
+datasets.dump_svmlight_file(xte, yte, te_f)
+
+# create connectors
+xtr_arr, xte_arr = ArrayConnector(xtr, ytr), ArrayConnector(xte, yte)
+xtr_svm, xte_svm = SVMConnector(tr_f), SVMConnector(te_f)
+optimizer = GenericSolver(solver_type='SGD', iterations=500, base_lr=0.1)
+
+# train model
+clf = MLP(sname=snames, repository=model_repo, **params)
+clf.fit(xtr_svm, validation_data=[xte_svm], solver=optimizer)
+
+# predict and show metrics
+ytr_pred, yte_pred = clf.predict(xtr_svm), clf.predict(xte_svm)
+report = metrics.classification_report(yte, yte_pred)
+print(report)
+
+# remove create files and folders
+os_utils._remove_files([tr_f, te_f])
+os_utils._remove_dirs([model_repo])
---
+++ examples/train_from_lmdb.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Ardalan MEHRANI <ardalan77400@gmail.com>
+@brief:
+"""
+import os
+import numpy as np
+from pydd.solver import GenericSolver
+from pydd.models import MLP
+from pydd.connectors import SVMConnector
+from pydd.utils import os_utils, lmdb_utils
+from sklearn import datasets, metrics, model_selection, preprocessing
+
+
+# Parameters
+seed = 1337
+np.random.seed(seed)  # for reproducibility
+n_classes = 10
+params = {"port": 8085, "nclasses": n_classes, "gpu": True}
+split_params = {"test_size": 0.2, "random_state": seed}
+
+
+# create dataset
+X, y = datasets.load_digits(n_class=n_classes, return_X_y=True)
+X = preprocessing.StandardScaler().fit_transform(X)
+xtr, xte, ytr, yte = model_selection.train_test_split(X, y, **split_params)
+
+
+# create and save train.svm and test.svm
+tr_f = os.path.abspath('x_train.svm')
+te_f = os.path.abspath('x_test.svm')
+datasets.dump_svmlight_file(xtr, ytr, tr_f)
+datasets.dump_svmlight_file(xte, yte, te_f)
+
+
+# create lmdb dataset
+tr_lmdb, te_lmdb = os.path.abspath('train.lmdb'), os.path.abspath('test.lmdb')
+vocab_path = os.path.abspath('vocab.dat')
+lmdb_utils.create_lmdb_from_svm(tr_f, tr_lmdb, vocab_path, **params)
+lmdb_utils.create_lmdb_from_svm(te_f, te_lmdb, **params)
+
+
+# create lmdb connectors
+train_data = SVMConnector(path=tr_f, lmdb_path=tr_lmdb, vocab_path=vocab_path)
+test_data = SVMConnector(path=te_f, lmdb_path=te_lmdb)
+
+
+# Training model from lmdb data
+clf = MLP(**params)
+optimizer = GenericSolver(solver_type='SGD', iterations=500, base_lr=0.01)
+logs = clf.fit(train_data, validation_data=[test_data], solver=optimizer)
+
+yte_pred = clf.predict(test_data)
+report = metrics.classification_report(yte, yte_pred)
+print(report)
+
+os_utils._remove_files([tr_f, te_f])
