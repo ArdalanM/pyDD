c362d006aef2052cd76659e2a640fb2750803f49
ArdalanM <ardalan77400@gmail.com>
2017-03-27 15:46:54 +0200

    update example: prediction_from_model

--- examples/prediction_from_model.py
+++ examples/prediction_from_model.py
@@ -14,7 +14,7 @@ from sklearn import datasets, preprocessing, model_selection, metrics
 # Parameters
 test_size = 0.2
 seed = 1337
-snames = 'predict_from_model'
+sname = 'predict_from_model'
 model_repo = os.path.abspath('trained_model')
 params = {'host': 'localhost', 'port': 8085, 'nclasses': 10, 'layers': [100, 100]}
 
@@ -22,7 +22,6 @@ params = {'host': 'localhost', 'port': 8085, 'nclasses': 10, 'layers': [100, 100
 if os.path.exists(model_repo):
     os_utils._remove_dirs([model_repo])
 os_utils._create_dirs([model_repo])
-
 
 # create dataset
 X, Y = datasets.load_digits(return_X_y=True)
@@ -36,13 +35,16 @@ datasets.dump_svmlight_file(xtr, ytr, tr_f)
 datasets.dump_svmlight_file(xte, yte, te_f)
 
 # create connectors
-xtr_arr, xte_arr = ArrayConnector(xtr, ytr), ArrayConnector(xte, yte)
 xtr_svm, xte_svm = SVMConnector(tr_f), SVMConnector(te_f)
-optimizer = GenericSolver(solver_type='SGD', iterations=500, base_lr=0.1)
+optimizer = GenericSolver(solver_type='SGD', iterations=500, base_lr=0.1, snapshot=100)
 
-# train model
-clf = MLP(sname=snames, repository=model_repo, **params)
-clf.fit(xtr_svm, validation_data=[xte_svm], solver=optimizer)
+# train model and delete model
+clf = MLP(sname=sname, repository=model_repo, **params)
+clf.fit(xtr_svm, validation_data=[xte_svm, xtr_svm], solver=optimizer)
+del clf
+
+params = {'host': 'localhost', 'port': 8085, 'nclasses': 10, 'finetuning': True, 'template': None}
+clf = MLP(sname=sname, repository=model_repo, **params)
 
 # predict and show metrics
 ytr_pred, yte_pred = clf.predict(xtr_svm), clf.predict(xte_svm)
