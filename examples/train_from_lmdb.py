a0804e3fe2621ed3677b1b4118d0c4d558c610cd
ArdalanM <ardalan77400@gmail.com>
2017-03-27 15:35:14 +0200

    update example: train_from_lmdb

--- examples/train_from_lmdb.py
+++ examples/train_from_lmdb.py
@@ -19,23 +19,27 @@ n_classes = 10
 params = {"port": 8085, "nclasses": n_classes, "gpu": True}
 split_params = {"test_size": 0.2, "random_state": seed}
 
+folder = "train-from-lmdb"
+if os.path.exists(folder):
+    os_utils._remove_dirs([folder])
+os_utils._create_dirs([folder])
+
 
 # create dataset
 X, y = datasets.load_digits(n_class=n_classes, return_X_y=True)
 X = preprocessing.StandardScaler().fit_transform(X)
 xtr, xte, ytr, yte = model_selection.train_test_split(X, y, **split_params)
 
-
 # create and save train.svm and test.svm
-tr_f = os.path.abspath('x_train.svm')
-te_f = os.path.abspath('x_test.svm')
+tr_f = os.path.abspath("{}/x_train.svm".format(folder))
+te_f = os.path.abspath("{}/x_test.svm".format(folder))
 datasets.dump_svmlight_file(xtr, ytr, tr_f)
 datasets.dump_svmlight_file(xte, yte, te_f)
 
-
 # create lmdb dataset
-tr_lmdb, te_lmdb = os.path.abspath('train.lmdb'), os.path.abspath('test.lmdb')
-vocab_path = os.path.abspath('vocab.dat')
+tr_lmdb = os.path.abspath("{}/train.lmdb".format(folder))
+te_lmdb = os.path.abspath("{}/test.lmdb".format(folder))
+vocab_path = os.path.abspath("{}/vocab.dat".format(folder))
 lmdb_utils.create_lmdb_from_svm(tr_f, tr_lmdb, vocab_path, **params)
 lmdb_utils.create_lmdb_from_svm(te_f, te_lmdb, **params)
 
@@ -54,4 +58,4 @@ yte_pred = clf.predict(test_data)
 report = metrics.classification_report(yte, yte_pred)
 print(report)
 
-os_utils._remove_files([tr_f, te_f])
+os_utils._remove_dirs([folder])
