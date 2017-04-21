a9257f9ca9251dbad24cf9e456bdfa9a0e4a1e33
Evgeny Bazarov <evgeny.bazarov@iosquare.com>
2017-04-21 11:58:33 +0200

    Adding LMDB connector

--- pydd/connectors.py
+++ pydd/connectors.py
@@ -38,6 +38,21 @@ class SVMConnector(Connectors):
                                            vocab_path=vocab_path)
 
 
+class LMDBConnector(Connectors):
+    """
+    """
+    def __init__(self, path, lmdb_path="", vocab_path=""):
+        self.name = "lmdb"
+
+        if path:
+            if not os.path.exists(path):
+                print("warning: {} does not exist".format(path))
+
+        super(LMDBConnector, self).__init__(path=path,
+                                            lmdb_path=lmdb_path,
+                                            vocab_path=vocab_path)
+
+
 class ArrayConnector(Connectors):
 
     def __init__(self, X, Y=None):
