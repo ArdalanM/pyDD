ec9c1006d2087e63ded7da693f4ee8b858537904
YaYaB <bezzayassine@gmail.com>
2017-07-28 15:50:01 +0200

    Manage a folder of logs

--- pydd/utils/viz.py
+++ pydd/utils/viz.py
@@ -12,7 +12,8 @@ import numpy as np
 import time
 import argparse
 import json
-
+import glob
+import os
 from visdom import Visdom
 
 
@@ -52,68 +53,73 @@ def batch_read(thefile):
 
 
 def main():
+    # Read args
     opt = get_args()
-   
-    vis = Visdom(env=opt.log_dir)
+    logs = glob.glob(os.path.join(opt.log_dir, '*.txt'))
+    if len(logs) == 0:
+        logs = [opt.log_dir]
 
-    # Create empty plot for each metrics
-    dic_plots = {}
-    for metric in METRICS:
-        dic_plots[metric] = vis.line(np.array([0]), np.array([0]),
-                        opts=dict(legend=False, xlabel='Iteration', title=metric,
-                                marginleft=30, marginright=30, marginbottom=30, margintop=30), win=metric, env=opt.log_dir)
+    for log in logs:
+        vis = Visdom(env=log)
 
-    # where the monitoring begins
-    curr_iter, prev_iter = 0, 0
-    with open(opt.log_dir) as f:
-
-        # Batch read
-        dic_metrics = {k:[] for k in METRICS}
-        iterations = []
-        lines = batch_read(f)
-        for line in lines:
-            try:
-                line = json.loads(line.replace('\'', '\"'))
-                curr_iter = line['iteration']
-
-                # print(line)
-                if curr_iter > prev_iter:
-                    iterations.append(curr_iter)
-                    prev_iter = curr_iter
-
-                    for metric in METRICS:
-                        if metric in line:
-                            dic_metrics[metric].append(line[metric])
-            except:
-                pass
-
+        # Create empty plot for each metrics
+        dic_plots = {}
         for metric in METRICS:
-            x = np.array(iterations)
-            y = np.array(dic_metrics[metric])
+            dic_plots[metric] = vis.line(np.array([0]), np.array([0]),
+                            opts=dict(legend=False, xlabel='Iteration', title=metric,
+                                    marginleft=30, marginright=30, marginbottom=30, margintop=30), win=metric, env=log)
 
-            if len(y) > 0:
-                vis.line(y, x, win=dic_plots[metric], update='append', env=opt.log_dir)
-        # END OF Batch read
+        # where the monitoring begins
+        curr_iter, prev_iter = 0, 0
+        with open(log) as f:
 
-        # stream read
-        print("stream mode...")
-        generator = stream_read(f)
-        while True:
-            line = generator.__next__()
+            # Batch read
+            dic_metrics = {k:[] for k in METRICS}
+            iterations = []
+            lines = batch_read(f)
+            for line in lines:
+                try:
+                    line = json.loads(line.replace('\'', '\"'))
+                    curr_iter = line['iteration']
 
-            try:
-                line = json.loads(line.replace('\'', '\"'))
-                curr_iter = line['iteration']
-                if curr_iter > prev_iter:
-                    prev_iter = curr_iter
-                    for metric in METRICS:
-                        if metric in line:
-                            x = np.array([curr_iter])
-                            y = np.array([line[metric]])
-                            vis.line(y, x, win=dic_plots[metric], update='append', env=opt.log_dir)
-            except:
-                pass
-        # END OF stream read
+                    # print(line)
+                    if curr_iter > prev_iter:
+                        iterations.append(curr_iter)
+                        prev_iter = curr_iter
+
+                        for metric in METRICS:
+                            if metric in line:
+                                dic_metrics[metric].append(line[metric])
+                except:
+                    pass
+
+            for metric in METRICS:
+                x = np.array(iterations)
+                y = np.array(dic_metrics[metric])
+
+                if len(y) > 0:
+                    vis.line(y, x, win=dic_plots[metric], update='append', env=log)
+            # END OF Batch read
+
+            # stream read
+            print("stream mode...")
+            generator = stream_read(f)
+            while True:
+                line = generator.__next__()
+
+                try:
+                    line = json.loads(line.replace('\'', '\"'))
+                    curr_iter = line['iteration']
+                    if curr_iter > prev_iter:
+                        prev_iter = curr_iter
+                        for metric in METRICS:
+                            if metric in line:
+                                x = np.array([curr_iter])
+                                y = np.array([line[metric]])
+                                vis.line(y, x, win=dic_plots[metric], update='append', env=log)
+                except:
+                    pass
+            # END OF stream read
 
 
 if __name__ == "__main__":
